7eb9ad0324fa1867b9ef2c9b1b396a86b72c6edd
Maciej Wódke <maciej@mwpro.pl>
2019-01-15 00:25:51 +0100

    Add spring boilerplate

---
+++ tiny-money-backend/src/main/java/com/mwpro/tinymoney/TinyMoneyApplication.java
@@ -0,0 +1,14 @@
+package com.mwpro.tinymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyMoneyApplication.class, args);
+    }
+
+}
+
---
+++ tiny-money-backend/src/test/java/com/mwpro/tinymoney/TinyMoneyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mwpro.tinymoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyMoneyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
