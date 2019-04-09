24d0b3b36dfaca03365b0880e0c9ee3f8e6a1a4b
Maciej Wódke <maciej@mwpro.pl>
2019-04-09 02:41:34 +0200

    Update TinyMoneyApplication.java

--- tiny-money-backend/src/main/java/com/mwpro/tinymoney/TinyMoneyApplication.java
+++ tiny-money-backend/src/main/java/com/mwpro/tinymoney/TinyMoneyApplication.java
@@ -3,15 +3,22 @@ package com.mwpro.tinymoney;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class TinyMoneyApplication {
+public class TinyMoneyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TinyMoneyApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TinyMoneyApplication.class);
     }
 
     @Bean
@@ -19,4 +26,3 @@ public class TinyMoneyApplication {
         return new ModelMapper();
     }
 }
-
