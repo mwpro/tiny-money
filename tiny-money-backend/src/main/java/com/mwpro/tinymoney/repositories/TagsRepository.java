2e329c2a313f3e982610f13923216378e09cb08b
Maciej Wódke <maciej@mwpro.pl>
2021-12-29 20:17:43 +0100

    Remove java backend (#318)

--- tiny-money-backend/src/main/java/com/mwpro/tinymoney/repositories/TagsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mwpro.tinymoney.repositories;
-
-import com.mwpro.tinymoney.models.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagsRepository extends JpaRepository<Tag, Integer> {
-}
