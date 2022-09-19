4c47e9fcd8e41608096e1f757868821742e82a7c
Maciej Wódke <maciej@mwpro.pl>
2022-09-19 22:09:27 +0200

    Quick filters in report UI

--- frontend/src/reports/ReportsFilter.vue
+++ frontend/src/reports/ReportsFilter.vue
@@ -8,11 +8,14 @@
       >
         <v-layout fill-height>
           <v-flex xs12 align-end flexbox>
+            <div>
+              <v-btn v-for="filter in quickFilters" @click="quickFilter(filter)">{{filter.name}}</v-btn>
+              <v-btn @click="monthPickerVisible = true" v-show="!monthPickerVisible">Wybierz miesiące</v-btn>
+            </div>
             <v-treeview :items="availableMonths" selectable v-model="selectedMonths" v-show="monthPickerVisible"></v-treeview>
+            <v-btn @click="applyFilters()" v-show="monthPickerVisible">Zastosuj</v-btn><br>
             Wybrane miesiące:
-            <span v-for="m in selectedMonthsUi"> {{ m.year }}-{{m.month}} </span>
-            <a @click="monthPickerVisible = true" v-show="!monthPickerVisible">Zmień</a><br>
-            <v-btn flat @click="applyFilters()">Zastosuj</v-btn>
+            <v-chip v-for="m in selectedMonthsUi">{{ m.year }}-{{m.month}}</v-chip>
           </v-flex>
         </v-layout>
       </v-container>
@@ -26,6 +29,14 @@
       return {
         selectedMonths: [],
         monthPickerVisible: false,
+        quickFilters: [
+          {name: "Cała historia", func: x => true},
+          {name: "Ten miesiąc", func: x => x.month == new Date().getMonth()+1 && x.year == new Date().getFullYear()},
+          {name: "Poprzedni miesiąc", func: x => x.month == new Date().getMonth() && x.year == new Date().getFullYear()},
+          {name: "Ten rok", func: x =>  x.year == new Date().getFullYear()},
+          {name: "Poprzedni rok", func: x => x.year == new Date().getFullYear()-1},
+          {name: "Ten i poprzedni rok", func: x => x.year == new Date().getFullYear() || x.year == new Date().getFullYear() -1}
+        ]
       }
     },
     created() {
@@ -52,7 +63,7 @@
             id: year,
             name: year,
             children: months.map(m => {
-              return {id: `${year}-${m}`, name: m}
+              return {id: `${year}-${m}`, name: m, month: m, year: year}
             })
           };
         });
@@ -60,8 +71,17 @@
     },
     methods: {
       applyFilters() {
+        if (!this.selectedMonths.length)
+          return;
         this.$store.dispatch('reports/setSelectedMonths', this.selectedMonthsUi)
         this.monthPickerVisible = false;
+      },
+      quickFilter(filter){
+        this.selectedMonths = this.availableMonths
+          .flatMap(y => y.children)
+          .filter(m => filter.func(m))
+          .map(m => m.id);
+        this.applyFilters();
       }
     }
   }
