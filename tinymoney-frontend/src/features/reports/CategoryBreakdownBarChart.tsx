5ed123c70f4c87a2b104d2791a03fb6d9c9c57ec
Maciej Wódke <maciej@mwpro.pl>
2026-02-04 00:40:42 +0100

    clickable legend in CategoryBreakdownBarChart

--- tinymoney-frontend/src/features/reports/CategoryBreakdownBarChart.tsx
+++ tinymoney-frontend/src/features/reports/CategoryBreakdownBarChart.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/chart"
 import type {ReportCategory} from "@/lib/api.ts";
 import {Bar, BarChart, CartesianGrid, XAxis, YAxis} from "recharts";
+import {useState} from "react";
 
 const chartConfig = {
 } satisfies ChartConfig
@@ -30,6 +31,7 @@ export function CategoryBreakdownBarChart({categories}: CategoryBreakdownPieChar
         return row;
     });
     const categoryNames = categories.map(cat => cat.categoryName);
+    const [activeCategories, setActiveCategories] = useState(categoryNames);
     const COLORS = [
         "#2563eb", "#16a34a", "#dc2626", "#d97706", "#7c3aed",
         "#0891b2", "#4b5563", "#db2777", "#ea580c", "#9333ea",
@@ -53,10 +55,30 @@ export function CategoryBreakdownBarChart({categories}: CategoryBreakdownPieChar
                         />
                         <YAxis />
                         <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-                        <ChartLegend />
+                        <ChartLegend onClick={(d, _, e) => {
+                            console.log(e);
+                            const categoryKey = d.dataKey;
+                            if (!categoryKey || typeof categoryKey !== "string") {
+                                return;
+                            }
+                            
+                            setActiveCategories(prev => {
+                                if (e.ctrlKey) {
+                                    return [categoryKey];
+                                }
+                                if (prev.length == 1 && prev.includes(categoryKey))
+                                    return categoryNames;
+                                if (prev.includes(categoryKey)) {
+                                    return prev.filter(el => el !== categoryKey);
+                                } else {
+                                    return [...prev, categoryKey];
+                                }
+                            });
+                        }} />
                         {categoryNames.map((categoryName, index) => (
                             <Bar
                                 key={categoryName}
+                                hide={!activeCategories.includes(categoryName)}
                                 dataKey={categoryName}
                                 stackId="a"
                                 name={categoryName}
