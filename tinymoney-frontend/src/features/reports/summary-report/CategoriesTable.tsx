1e3992afe98e96358fd0ca9e69a621f88190296d
Maciej Wódke <maciej@mwpro.pl>
2026-02-16 01:21:20 +0100

    Subtle bg for categories report table

--- tinymoney-frontend/src/features/reports/summary-report/CategoriesTable.tsx
+++ tinymoney-frontend/src/features/reports/summary-report/CategoriesTable.tsx
@@ -30,7 +30,7 @@ export function CategoriesTable({categories, reportSettings}: BudgetTableProps) 
                 <TableBody>
                     {categories.map((category) => (
                         <Fragment key={category.categoryId}>
-                            {categories.length > 1 && <TableRow key={category.categoryId}>
+                            {categories.length > 1 && <TableRow key={category.categoryId} className="bg-gray-100">
                                 <TableCell onClick={() => setExpandedCategory(prev => prev !== category.categoryId ? category.categoryId : undefined)}
                                            className={`font-bold`}>{category.categoryName}</TableCell>
                                 {category.periods.map(period => (<TableCell key={period.periodLabel} className="text-right">
