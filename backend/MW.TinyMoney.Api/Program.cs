eeef27d6ee078a91f7b88fccfc954576fc0d8866
Maciej Wódke <maciej@mwpro.pl>
2026-02-23 22:33:45 +0100

    .NET 10

--- backend/MW.TinyMoney.Api/Program.cs
+++ backend/MW.TinyMoney.Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using MW.TinyMoney.Api.Budget;
 using MW.TinyMoney.Api.Buffer;
 using MW.TinyMoney.Api.Buffer.Parsers;
