913ca70acac9e41ac28800d781d19692dd5022e2
Maciej Wódke <maciej@mwpro.pl>
2026-03-11 00:01:27 +0100

    Code review

--- backend/MW.TinyMoney.Api/Reports/ReportsController.cs
+++ backend/MW.TinyMoney.Api/Reports/ReportsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace MW.TinyMoney.Api.Reports
     {
         [HttpGet("summary-report")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(SummaryReportModel))]
-        public async Task<IActionResult> GetSummaryReport([FromServices] ISummaryReport summaryReport, [FromQuery] System.DateTime? dateFrom, [FromQuery] System.DateTime? dateTo, [FromQuery] bool splitByMonth)
+        public async Task<IActionResult> GetSummaryReport([FromServices] ISummaryReport summaryReport, [FromQuery] DateTime? dateFrom, [FromQuery] DateTime? dateTo, [FromQuery] bool splitByMonth)
         {
             var reportData = await summaryReport.Prepare(dateFrom, dateTo, splitByMonth);
             return Ok(reportData);
@@ -19,7 +20,7 @@ namespace MW.TinyMoney.Api.Reports
 
         [HttpGet("top-list-report")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(TopListReportModel))]
-        public async Task<IActionResult> GetToplistReport([FromServices] ITopListReport topListReport, [FromQuery] System.DateTime? dateFrom, [FromQuery] System.DateTime? dateTo, [Required, Range(1, 200)] int numberOfTopEntries)
+        public async Task<IActionResult> GetToplistReport([FromServices] ITopListReport topListReport, [FromQuery] DateTime? dateFrom, [FromQuery] DateTime? dateTo, [Required, Range(1, 200)] int numberOfTopEntries)
         {
             var reportData = await topListReport.Prepare(dateFrom, dateTo, numberOfTopEntries);
             return Ok(reportData);
@@ -27,7 +28,7 @@ namespace MW.TinyMoney.Api.Reports
 
         [HttpGet("sankey-report")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(SankeyReportModel))]
-        public async Task<IActionResult> GetSankeyReport([FromServices] ISankeyReport sankeyReport, [FromQuery] System.DateTime? dateFrom, [FromQuery] System.DateTime? dateTo)
+        public async Task<IActionResult> GetSankeyReport([FromServices] ISankeyReport sankeyReport, [FromQuery] DateTime? dateFrom, [FromQuery] DateTime? dateTo)
         {
             var reportData = await sankeyReport.Prepare(dateFrom, dateTo);
             return Ok(reportData);
