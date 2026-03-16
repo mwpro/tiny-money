2a82daf036a14bf541b191bc98315581cbc4b93b
Maciej Wódke <maciej@mwpro.pl>
2026-03-16 23:11:42 +0100

    Cleanup

--- backend/MW.TinyMoney.Api/Import/ImportController.cs
+++ backend/MW.TinyMoney.Api/Import/ImportController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Sentry;
 
 namespace MW.TinyMoney.Api.Import;
 
