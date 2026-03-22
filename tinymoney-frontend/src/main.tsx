50e1d18250f5a37682daff40fffa9aa56a509fe1
Maciej Wódke <maciej@mwpro.pl>
2026-03-22 13:18:42 +0100

    Fix redirect after auth for browsers with cookie protection/ITP

--- tinymoney-frontend/src/main.tsx
+++ tinymoney-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {Toaster} from "sonner";
 import {Auth0Provider} from "@auth0/auth0-react";
+import type {AppState} from "@auth0/auth0-react";
 import {ApiClientProvider} from "@/api/ApiClientProvider.tsx";
 import {ConfigurationProvider} from "@/ConfigurationContext.tsx";
 import * as Sentry from "@sentry/react";
@@ -29,12 +30,16 @@ if (config.sentryDsn) {
 
 const queryClient = new QueryClient()
 
+function onRedirectCallback(appState?: AppState) {
+    window.history.replaceState({}, document.title, appState?.returnTo ?? window.location.pathname);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider domain={config.auth0Domain} clientId={config.auth0ClientId} authorizationParams={{
             redirect_uri: window.location.origin,
             audience: config.auth0Audience
-        }}>
+        }} onRedirectCallback={onRedirectCallback}>
             <ConfigurationProvider configuration={config}>
                 <ApiClientProvider configuration={config}>
                     <Toaster />
